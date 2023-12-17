03e2b6db78c83623a0c2cad288ed392df4d9e547
Nicolas Stadler <stadlernicolas26@gmail.com>
2023-12-17 17:43:15 +0100

    db triggers

--- backend/src/db/triggers/FileSubscriber.ts
+++
@@ -1,6 +0,0 @@
-import { EventArgs, EventSubscriber } from '@mikro-orm/core';
-import { File } from 'src/db/entities/File';
-
-export class FileSubscriber implements EventSubscriber<File> {
-	async afterCreate(args: EventArgs<File>): Promise<void> {}
-}
