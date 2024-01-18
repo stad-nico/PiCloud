f0b6da810f042f39fa57cacfdd41c080cd6886b9
Nicolas Stadler <stadlernicolas26@gmail.com>
2024-01-18 11:33:27 +0100

    fix some tests

--- backend/test/unit/EnvConfig.test.ts
+++ backend/test/unit/EnvConfig.test.ts
@@ -5,39 +5,37 @@ describe('env.config.ts', () => {
 	describe('validate', () => {
 		const config: Record<string, unknown> = {
 			PORT: 3000,
-			DISK_STORAGE_PATH: 'storagePath',
-			DISK_RECYCLE_PATH: 'recyclePath',
+			STORAGE_PATH: 'storagePath',
+			DB_HOST: 'dbHost',
+			DB_NAME: 'dbName',
+			DB_PASSWORD: 'dbPassword',
+			DB_PORT: 4000,
+			DB_USERNAME: 'dbUsername',
+			NODE_ENV: NodeEnv.Develop,
 		};
 
 		it('should validate config', () => {
 			expect(() => validate(config)).not.toThrow();
 			expect(validate(config)).toBeInstanceOf(EnvVariables);
-			expect({ ...validate(config) }).toStrictEqual({
-				PORT: 3000,
-				DISK_STORAGE_PATH: 'storagePath',
-				DISK_RECYCLE_PATH: 'recyclePath',
-				NODE_ENV: NodeEnv.Testing,
-			});
+			expect({ ...validate(config) }).toStrictEqual(config);
 		});
 
 		it('should have default value NodeEnv.Develop as NODE_ENV value in config', () => {
 			delete process.env.NODE_ENV;
+			delete config['NODE_ENV'];
 
 			expect({ ...validate(config) }).toStrictEqual({
-				PORT: 3000,
-				DISK_STORAGE_PATH: 'storagePath',
-				DISK_RECYCLE_PATH: 'recyclePath',
+				...config,
 				NODE_ENV: NodeEnv.Develop,
 			});
 		});
 
 		it('should have correct NODE_ENV value in config', () => {
+			delete config['NODE_ENV'];
 			process.env['NODE_ENV'] = 'test';
 
 			expect({ ...validate(config) }).toStrictEqual({
-				PORT: 3000,
-				DISK_STORAGE_PATH: 'storagePath',
-				DISK_RECYCLE_PATH: 'recyclePath',
+				...config,
 				NODE_ENV: NodeEnv.Testing,
 			});
 		});
---
+++ backend/test/unit/util/PathUtils.test.ts
@@ -0,0 +1,75 @@
+import * as fs from 'fs/promises';
+
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { PathUtils } from 'src/util/PathUtils';
+
+describe('PathUtils', () => {
+	let configService: ConfigService;
+
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				{
+					provide: ConfigService,
+					useValue: {
+						getOrThrow: () => {
+							return 'test';
+						},
+					},
+				},
+			],
+		}).compile();
+
+		module.useLogger(false);
+		configService = module.get(ConfigService);
+	});
+
+	describe('isPathRelative', () => {
+		it('should return true if path is a relative path that does not leave base dir', () => {
+			expect(PathUtils.isPathRelative(configService, 't/fff/t.txt')).toBe(true);
+			expect(PathUtils.isPathRelative(configService, './t/fff/t.txt')).toBe(true);
+			expect(PathUtils.isPathRelative(configService, '/t/fff/t.txt')).toBe(true);
+		});
+
+		it('should return true if path is a relative path in base dir', () => {
+			expect(PathUtils.isPathRelative(configService, '/t.txt')).toBe(true);
+			expect(PathUtils.isPathRelative(configService, './t.txt')).toBe(true);
+			expect(PathUtils.isPathRelative(configService, 't.txt')).toBe(true);
+		});
+
+		it('should return false if path leaves base dir', () => {
+			expect(PathUtils.isPathRelative(configService, '../t.txt')).toBe(false);
+			expect(PathUtils.isPathRelative(configService, './../t.txt')).toBe(false);
+			expect(PathUtils.isPathRelative(configService, './t/f/../../../f.txt')).toBe(false);
+		});
+	});
+
+	describe('pathExists', () => {
+		it('should return false because file does not exist', async () => {
+			(fs.access as jest.Mock).mockRejectedValue(new Error());
+
+			await expect(await PathUtils.pathExists('t.txt')).toBe(false);
+		});
+
+		it('should return true because file does exists', async () => {
+			(fs.access as jest.Mock).mockResolvedValue(undefined);
+
+			await expect(await PathUtils.pathExists('C:')).toBe(true);
+		});
+	});
+
+	describe('join', () => {
+		it("should join the path with 'test' using path.join", () => {
+			expect(PathUtils.join(configService, 't.txt', Environment.DiskStoragePath)).toBe(path.join('test', 't.txt'));
+		});
+	});
+
+	describe('normalizePathForOS', () => {
+		it('should replace slashes with path.sep', () => {
+			expect(FileUtils['normalizePathForOS']('test/test.txt')).toBe('test/test.txt'.replaceAll(/(\/|\\)/gi, path.sep));
+			expect(FileUtils['normalizePathForOS']('test\\test.txt')).toBe('test/test.txt'.replaceAll(/(\/|\\)/gi, path.sep));
+		});
+	});
+});
