627d77f0eab52debc3bf6742de2c0313965e74ab
Nicolas Stadler <stadlernicolas26@gmail.com>
2024-05-21 19:58:30 +0200

    add default file icon

--- frontend/src/components/app/explorer/content-list/content-list-file/ContentListFileComponent.ts
+++ frontend/src/components/app/explorer/content-list/content-list-file/ContentListFileComponent.ts
@@ -1,9 +1,15 @@
 import { Component, Input } from '@angular/core';
 
+export enum FileExtension {
+	DEFAULT = 'default',
+	TXT = 'txt',
+}
+
 @Component({
 	selector: 'content-list-file',
 	standalone: true,
 	templateUrl: './ContentListFileComponent.html',
+	styleUrl: './ContentListFileComponent.css',
 })
 export class ContentListFileComponent {
 	@Input({ required: true })
@@ -11,4 +17,10 @@ export class ContentListFileComponent {
 
 	@Input({ required: true })
 	size!: number;
+
+	extension!: FileExtension;
+
+	ngOnInit() {
+		this.extension = /*(<any>FileExtension)[this.name.match(/(?!\.)([^\.]+)$/gim)![0].toUpperCase() as FileExtension] ?? */ FileExtension.DEFAULT;
+	}
 }
